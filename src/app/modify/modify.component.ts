9b442c37d41661ecd2e41b8b86b2e15d5e93b6b9
NotAUserName0 <dill1@live.com.mx>
2022-11-16 17:01:33 -0600

    add

--- src/app/modify/modify.component.ts
+++ src/app/modify/modify.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Alumno } from '../alumno.model';
+import { ListarService } from '../listar.service';
 
 @Component({
   selector: 'app-modify',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModifyComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('help')help:ElementRef;
+
+  alumno : Alumno[];
+  res : number;
+
+  @ViewChild('err')err:ElementRef; //referencia a objeto del html
+  @ViewChild('tab')tab:ElementRef;
+
+  constructor(private listarService : ListarService) { 
+    
+  }
 
   ngOnInit(): void {
   }
+
+  displayHelp(){
+    console.log("entro nya");
+    this.help.nativeElement.style.display = 'block';
+  }
+
+  obternerAlumnos(){
+    this.listarService.obtenerAlumnos().subscribe(
+      respuesta => {
+        this.res = Object.keys(respuesta).length;
+        if(this.res >= 1){
+          this.alumno = respuesta;
+        }else{
+          this.err.nativeElement.style.display = 'block';
+          this.tab.nativeElement.style.display = 'none';
+        }
+      }
+    )
+  }
+
+  
 
 }
